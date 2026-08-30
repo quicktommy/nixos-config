6cbf576fcf5ecee6723f0dc5dcda19cb0a7b6b53
tommy <tomas.duraes2009@gmail.com>
2026-08-30 19:00:21 +0100

    Separate machine-specific monitor configuration

--- home/hypr/config/monitors.lua
+++ home/hypr/config/monitors.lua
@@ -1,22 +1,15 @@
 -- Monitor wiki https://wiki.hypr.land/Configuring/Basics/Monitors/
--- Example: output can be found with hyprctl monitors. Edit variables.lua for the monitor outputs instead of here directly
--- hl.monitor({
---     output    = "MONITOR1",
---     mode      = "1920x1080@60",
---     position  = "0x0",
---     scale     = "1",
--- })
 
 hl.monitor({
-    output    = MONITOR2, -- eDP-1
-    mode      = "1920x1080@59.98",
-    position  = "0x360",
-    scale     = "1.5",
+    output   = MONITOR2,
+    mode     = MONITOR2_MODE,
+    position = MONITOR2_POSITION,
+    scale    = MONITOR2_SCALE,
 })
 
 hl.monitor({
-    output    = MONITOR1, -- HDMI-A-1
-    mode      = "1920x1080@100",
-    position  = "1280x0",
-    scale     = "1",
+    output   = MONITOR1,
+    mode     = MONITOR1_MODE,
+    position = MONITOR1_POSITION,
+    scale    = MONITOR1_SCALE,
 })
--- home/hypr/config/variables.lua
+++ home/hypr/config/variables.lua
@@ -7,10 +7,7 @@ EDITOR       = "gnome-text-editor --new-window"
 CALCULATOR   = "gnome-calculator"
 
 -- Monitors
-MONITOR1 = "HDMI-A-1"
-MONITOR2 = "eDP-1"
-MONITOR3 = ""
-PRIMARY_MONITOR = MONITOR1
+dofile("/etc/nixos/local/monitors.lua")
 
 -- Workspaces
 NUM_WPM = 3 -- Number of workspaces per monitor (Max 10)
