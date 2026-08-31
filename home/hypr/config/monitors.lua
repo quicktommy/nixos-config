cec21669e69efad3c1f050a7c14f93d73fbdfcd2
tommy <tomas.duraes2009@gmail.com>
2026-08-31 14:58:44 +0100

    Add generic monitor configuration

--- home/hypr/config/monitors.lua
+++ home/hypr/config/monitors.lua
@@ -1,15 +1,14 @@
--- Monitor wiki https://wiki.hypr.land/Configuring/Basics/Monitors/
+-- Monitor configuration
+-- See: https://wiki.hypr.land/Configuring/Monitors/
+--
+-- Find monitor output names and supported modes with:
+-- hyprctl monitors
+--
+-- Set MONITOR1, MONITOR2, etc. in variables.lua.
 
 hl.monitor({
-    output   = MONITOR2,
-    mode     = MONITOR2_MODE,
-    position = MONITOR2_POSITION,
-    scale    = MONITOR2_SCALE,
-})
-
-hl.monitor({
-    output   = MONITOR1,
-    mode     = MONITOR1_MODE,
-    position = MONITOR1_POSITION,
-    scale    = MONITOR1_SCALE,
+    output   = "",
+    mode     = "preferred",
+    position = "0x0",
+    scale    = "1",
 })
--- home/hypr/config/variables.lua
+++ home/hypr/config/variables.lua
@@ -7,7 +7,11 @@ EDITOR       = "gnome-text-editor --new-window"
 CALCULATOR   = "gnome-calculator"
 
 -- Monitors
-dofile("/etc/nixos/local/monitors.lua")
+-- Set these to your monitor output names from `hyprctl monitors`
+MONITOR1 = ""
+MONITOR2 = ""
+MONITOR3 = ""
+PRIMARY_MONITOR = MONITOR1
 
 -- Workspaces
 NUM_WPM = 3 -- Number of workspaces per monitor (Max 10)
